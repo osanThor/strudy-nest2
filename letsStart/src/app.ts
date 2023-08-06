fc88ca1e4d4f56b64af63cc141a3193e660d029c
junyoung <jadw9715@gmail.com>
2023-08-06 22:37:47 +0900

    feat. express middleware

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -3,12 +3,35 @@ import { Cat, CatType } from "./app.model";
 
 const app: express.Express = express();
 
-const data = [1, 2, 3, 4];
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is middleware");
+  next();
+});
+app.get("/cats/som", (req, res, next) => {
+  console.log("this is Som middleware");
+  next();
+});
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send({ cats: Cat });
 });
 
+app.get(
+  "/cats/blue",
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.send({ blue: Cat[0] });
+  }
+);
+app.get("/cats/som", (req: express.Request, res: express.Response) => {
+  res.send({ som: Cat[1] });
+});
+
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is error middleware");
+  res.send({ error: "404 not found error" });
+});
 app.listen(8000, () => {
   console.log("Server is On...");
 });
