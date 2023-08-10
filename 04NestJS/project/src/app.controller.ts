c55e965239db68e78db6a8290217bcfa650b942a
junyoung <jadw9715@gmail.com>
2023-08-10 22:53:23 +0900

    Feat. nest controller partern

--- 04NestJS/project/src/app.controller.ts
+++ 04NestJS/project/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
-@Controller()
+@Controller('cats')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('hello/:id/:name')
+  getHello(
+    @Req() req: Request,
+    @Body() Bady,
+    @Param() params: { id: string; name: string },
+  ): string {
+    // console.log(req);
+    // console.log(Bady);
+    // console.log(params);
     return this.appService.getHello();
   }
 }
