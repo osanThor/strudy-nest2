fe9810636d2e56f8a1114a6da0089cb903faebe0
junyoung <jadw9715@gmail.com>
2023-08-21 23:27:24 +0900

    Feat. 모듈과 캡슐화

--- 04NestJS/project/src/app.controller.ts
+++ 04NestJS/project/src/app.controller.ts
@@ -1,20 +1,17 @@
 import { Body, Controller, Get, Param, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Request } from 'express';
+import { CatsService } from './cats/cats.service';
 
 @Controller('cats')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly catsService: CatsService,
+  ) {}
 
-  @Get('hello/:id/:name')
-  getHello(
-    @Req() req: Request,
-    @Body() Bady,
-    @Param() params: { id: string; name: string },
-  ): string {
-    // console.log(req);
-    // console.log(Bady);
-    // console.log(params);
-    return this.appService.getHello();
+  @Get()
+  getHello() {
+    return 'hello world';
   }
 }
