a0ed769517b48d0370ff4a3a44e38fda32d48649
junyoung <jadw9715@gmail.com>
2023-08-21 23:45:54 +0900

    Feat. nest middleware

---
+++ 05NestJS/project/src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Get, Param, Req } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Request } from 'express';
+import { CatsService } from './cats/cats.service';
+
+@Controller('cats')
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private readonly catsService: CatsService,
+  ) {}
+
+  @Get()
+  getHello() {
+    return this.catsService.hiCatService();
+  }
+}
---
+++ 05NestJS/project/src/cats/cats.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+  @Get()
+  getAllcat() {
+    return 'all cat';
+  }
+
+  @Get(':id')
+  getOneCat() {
+    return 'one cat';
+  }
+
+  @Post()
+  createCat() {
+    return 'create cat';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updatePartialCat() {
+    return 'update';
+  }
+  @Delete()
+  deleteCat() {
+    return 'delete cat';
+  }
+}
