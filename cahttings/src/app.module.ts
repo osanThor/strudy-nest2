fd466126b130145b7fefbeda772aeb37de305675
junyoung <jadw9715@gmail.com>
2023-10-24 22:48:17 +0900

    setting. websocket nest

--- cahttings/src/app.module.ts
+++ cahttings/src/app.module.ts
@@ -2,12 +2,14 @@ import { ConfigModule } from '@nestjs/config';
 import { Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ChatsModule } from './chats/chats.module';
 import mongoose from 'mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGODB_URI, {}),
+    ChatsModule,
   ],
   controllers: [AppController],
   providers: [],
