6592da6259877eba42368678208dfbbdfb38fe69
junyoung <jadw9715@gmail.com>
2023-10-30 21:34:21 +0900

    feat. socket io

--- cahttings/public/js/scripts.js
+++ cahttings/public/js/scripts.js
@@ -6,14 +6,18 @@ const helloStrangerElement = getElementById('hello_stranger');
 const chattingBoxElement = getElementById('chatting_box');
 const formElement = getElementById('chat_form');
 
+socket.on('user_connected', (username) => {
+  console.log(`${username} connected`);
+});
+
+const drawHelloStranger = (username) => {
+  helloStrangerElement.innerText = `Hello ${username} Stranger :)`;
+};
+
 function helloUser() {
   const username = prompt('What is your name?');
   socket.emit('new_user', username, (data) => {
-    console.log(data);
-  });
-  console.log(username);
-  socket.on('hello_user', (data) => {
-    console.log(data);
+    drawHelloStranger(data);
   });
 }
 
